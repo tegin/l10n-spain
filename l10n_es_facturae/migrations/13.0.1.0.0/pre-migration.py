deb8060565324e8ab0d261acc5aaa1df7336e8a0
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-11-18 18:57:48 +0100

    [13.0][MIG] l10n_es_facturae

---
+++ l10n_es_facturae/migrations/13.0.1.0.0/pre-migration.py
@@ -0,0 +1,61 @@
+# Copyright 2020 Creu Blanca
+
+from openupgradelib import openupgrade
+
+models_to_rename = [
+    ("account.invoice.integration", "account.move.integration"),
+    ("account.invoice.integration.log", "account.move.integration.log"),
+    ("account.invoice.integration.method", "account.move.integration.method"),
+]
+
+tables_to_rename = [
+    ("account_invoice_integration", "account_move_integration"),
+    ("account_invoice_integration_log", "account_move_integration_log"),
+    ("account_invoice_integration_method", "account_move_integration_method"),
+    (
+        "account_invoice_integration_method_res_partner_rel",
+        "account_move_integration_method_res_partner_rel",
+    ),
+]
+
+column_renames = {
+    "account_move_integration_method_res_partner_rel": [
+        ("account_invoice_integration_method_id", "account_move_integration_method_id"),
+    ],
+}
+
+xmlids_to_rename = [
+    (
+        "l10n_es_facturae.seq_account_invoice_integration_log",
+        "l10n_es_facturae.seq_account_move_integration_log",
+    )
+]
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    cr = env.cr
+    openupgrade.rename_models(cr, models_to_rename)
+    openupgrade.rename_tables(cr, tables_to_rename)
+    openupgrade.rename_columns(env.cr, column_renames)
+
+    openupgrade.rename_xmlids(cr, xmlids_to_rename)
+
+    # Add move_id to the `account.invoice.integration` model.
+    openupgrade.logged_query(
+        env.cr,
+        """
+            ALTER TABLE account_move_integration
+            ADD COLUMN IF NOT EXISTS move_id INTEGER
+        """,
+    )
+    # Link the move_id to the old account invoice's move.
+    openupgrade.logged_query(
+        env.cr,
+        """
+            UPDATE account_move_integration aii
+            SET move_id = am.id
+            FROM account_move am
+            WHERE am.old_invoice_id = aii.invoice_id
+        """,
+    )
